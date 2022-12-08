4f7b7bc5a4061bbc74bd0646d4d44ed84628fb6b
SarpTekin <39226417+SarpTekin@users.noreply.github.com>
2022-12-09 01:27:40 +0300

    Update WsApplication.java

--- backend/src/main/java/com/socialise/ws/WsApplication.java
+++ backend/src/main/java/com/socialise/ws/WsApplication.java
@@ -2,8 +2,10 @@ package com.socialise.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.socialise.ws.user") 
 public class WsApplication {
 
 	public static void main(String[] args) {
